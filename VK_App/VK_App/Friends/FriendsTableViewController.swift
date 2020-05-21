b1e90dde9473c1d73b00ee92d5f07e51578b6be0
Nikolay Zhukov <nickolayzhukov@gmail.com>
2020-05-21 22:55:13 +0300

    Lesson4v2

--- VK_App/VK_App/Friends/FriendsTableViewController.swift
+++ VK_App/VK_App/Friends/FriendsTableViewController.swift
@@ -8,93 +8,93 @@
 
 import UIKit
 
+struct FriendsSection {
+    var key: String
+    var friends: [User]
+}
+
 class FriendsTableViewController: UITableViewController {
     
-    var friends: [User] = [User(name: "Василий", avatar: UIImage(named: "VasiliyAvatar")!, photos: [UIImage(named: "VasiliyPhoto")!, UIImage(named: "VasiliyAvatar")!]),
-                           User(name: "Снежана", avatar: UIImage(named: "SnezhanaAvatar")!, photos: [UIImage(named: "SnezhanaPhoto")!, UIImage(named: "SnezhanaPhoto2")!, UIImage(named: "SnezhanaPhoto2")!])]
-        
+    var friends: [User] = [User(firstName: "Василий", lastName: "Марцыпанов", avatar:       UIImage(named: "VasiliyAvatar")!, photos: [UIImage(named: "VasiliyPhoto")!, UIImage(named: "VasiliyAvatar")!]),
+                           User(firstName: "Снежана", lastName: "Денисова", avatar: UIImage(named: "SnezhanaAvatar")!, photos: [UIImage(named: "SnezhanaPhoto")!, UIImage(named: "SnezhanaPhoto2")!]),
+                            User(firstName: "Lucas", lastName: "Rin", avatar: UIImage(named: "LucasAvatar")!, photos: [UIImage(named: "LucasPhoto1")!, UIImage(named: "LucasPhoto2")!]),
+                            User(firstName: "Ксения", lastName: "Морковкина", avatar: UIImage(named: "KseniaAvatar")!, photos: [UIImage(named: "KseniaPhoto1")!, UIImage(named: "KseniaPhoto2")!, UIImage(named: "KseniaPhoto3")!]),
+                            User(firstName: "Ксения", lastName: "Морковкина", avatar: UIImage(named: "KseniaAvatar")!, photos: [UIImage(named: "KseniaPhoto1")!, UIImage(named: "KseniaPhoto2")!, UIImage(named: "KseniaPhoto3")!]),
+                            User(firstName: "Ксения", lastName: "Морковкина", avatar: UIImage(named: "KseniaAvatar")!, photos: [UIImage(named: "KseniaPhoto1")!, UIImage(named: "KseniaPhoto2")!, UIImage(named: "KseniaPhoto3")!]),
+                            User(firstName: "Ксения", lastName: "Морковкина", avatar: UIImage(named: "KseniaAvatar")!, photos: [UIImage(named: "KseniaPhoto1")!, UIImage(named: "KseniaPhoto2")!, UIImage(named: "KseniaPhoto3")!]),
+                            User(firstName: "Ксения", lastName: "Морковкина", avatar: UIImage(named: "KseniaAvatar")!, photos: [UIImage(named: "KseniaPhoto1")!, UIImage(named: "KseniaPhoto2")!, UIImage(named: "KseniaPhoto3")!]),
+                            User(firstName: "Ксения", lastName: "Морковкина", avatar: UIImage(named: "KseniaAvatar")!, photos: [UIImage(named: "KseniaPhoto1")!, UIImage(named: "KseniaPhoto2")!, UIImage(named: "KseniaPhoto3")!]),
+                            User(firstName: "Ксения", lastName: "Морковкина", avatar: UIImage(named: "KseniaAvatar")!, photos: [UIImage(named: "KseniaPhoto1")!, UIImage(named: "KseniaPhoto2")!, UIImage(named: "KseniaPhoto3")!])]
+    
+    var friendsSection = [FriendsSection]()
+//    var filteredFriends = [User]()
+//    let searchController = UISearchController(searchResultsController: nil)
+//    var searchBarIsEmpty: Bool {
+//        guard let text = searchController.searchBar.text else { return false }
+//        return text.isEmpty
+//    }
+//    var isFiltering: Bool {
+//        return searchController.isActive && !searchBarIsEmpty
+//    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let friendsDict = Dictionary.init(grouping: friends){$0.lastName.prefix(1)}
+        friendsSection = friendsDict.map { FriendsSection(key: String($0.key), friends: $0.value) }
+        friendsSection.sort {$0.key < $1.key}
+        
+//        searchController.searchResultsUpdater = self
+//        searchController.obscuresBackgroundDuringPresentation = false
+//        searchController.searchBar.placeholder = "Search Friend"
+//        navigationItem.searchController = searchController
+//        definesPresentationContext = true
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         guard segue.identifier == "showPhotos" else { return }
         if let indexPath = self.tableView.indexPathForSelectedRow {
             let destination = segue.destination as! FriendsPhotosCollectionViewController
-            destination.photos = friends[indexPath.row].photos
+            destination.photos = friendsSection[indexPath.section].friends[indexPath.row].photos
         }
+}
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return friendsSection.count
     }
-
-    // MARK: - Table view data source
-
-
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return friendsSection.map { $0.key }[section]
+    }
+    
+    override func sectionIndexTitles(for tableView: UITableView) -> [String]? {
+        return friendsSection.map { $0.key }
+    }
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-            // #warning Incomplete implementation, return the number of rows
-        return friends.count
-    }
+        return friendsSection[section].friends.count
+}
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "FriendCell", for: indexPath) as! FriendTableViewCell
             
-        cell.friendName.text = friends[indexPath.row].name
-        cell.avatarView.avatar.image = friends[indexPath.row].avatar
-
-            // Configure the cell...
-
+        cell.friendName.text = friendsSection[indexPath.section].friends[indexPath.row].firstName + " " + friendsSection[indexPath.section].friends[indexPath.row].lastName
+        cell.avatarView.avatar.image = friendsSection[indexPath.section].friends[indexPath.row].avatar
         return cell
     }
         
 }
 
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+//extension FriendsTableViewController: UISearchResultsUpdating {
+//    func updateSearchResults(for searchController: UISearchController) {
+//        filterContentForSearch(searchController.searchBar.text!)
+//    }
+//    
+//    func filterContentForSearch(_ searchText: String) {
+//        filteredFriends = friends.filter({(friend: User) -> Bool in
+//            return friend.lastName.lowercased().contains(searchText.lowercased())
+//        })
+//    }
+//    
+//}
