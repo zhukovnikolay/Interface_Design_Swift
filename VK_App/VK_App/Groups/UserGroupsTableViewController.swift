7539609c96f448f91612b1154e7fefab87a5fb18
Nikolay Zhukov <nickolayzhukov@gmail.com>
2020-06-25 23:18:18 +0300

    Lesson2.4

---
+++ VK_App/VK_App/Groups/UserGroupsTableViewController.swift
@@ -0,0 +1,30 @@
+//
+//  UserGroupsTableViewController.swift
+//  VK_App
+//
+//  Created by Nikolay Zhukov on 25.06.2020.
+//  Copyright © 2020 Nikolay Zhukov. All rights reserved.
+//
+
+import UIKit
+
+class UserGroupsTableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
